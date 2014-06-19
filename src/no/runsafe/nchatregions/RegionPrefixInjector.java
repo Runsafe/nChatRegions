3e23af32b6ba3d7d9683150653b35a9f6678c99f
Morten Nilsen <morten@runsafe.no>
2014-06-20 00:06:14 +0200

    Make a separate plugin to handle region prefixes

---
+++ src/no/runsafe/nchatregions/RegionPrefixInjector.java
@@ -0,0 +1,56 @@
+package no.runsafe.nchatregions;
+
+import no.runsafe.framework.api.IConfiguration;
+import no.runsafe.framework.api.command.ICommandExecutor;
+import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.nchat.channel.IChannelManager;
+import no.runsafe.nchat.channel.ILocationTagManipulator;
+import no.runsafe.worldguardbridge.IRegionControl;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+
+public class RegionPrefixInjector implements ILocationTagManipulator, IConfigurationChanged
+{
+	public RegionPrefixInjector(IRegionControl worldGuard, IChannelManager manager)
+	{
+		this.worldGuard = worldGuard;
+		manager.registerLocationTagManip(this);
+	}
+
+	@Nonnull
+	@Override
+	public String getLocationTag(ICommandExecutor player, @Nonnull String tag)
+	{
+		return player instanceof IPlayer ? getRegionTag((IPlayer) player, tag) : tag;
+	}
+
+	@Nonnull
+	private String getRegionTag(@Nonnull IPlayer player, @Nonnull String tag)
+	{
+		String worldName = player.getWorldName();
+		List<String> regions = worldGuard.getRegionsAtLocation(player.getLocation());
+
+		if (regions != null)
+		{
+			for (String region : regions)
+			{
+				String regionName = worldName + '-' + region;
+				if (regionTags.containsKey(regionName))
+					return regionTags.get(regionName);
+			}
+		}
+		return tag;
+	}
+
+	@Override
+	public void OnConfigurationChanged(IConfiguration configuration)
+	{
+		regionTags = configuration.getConfigValuesAsMap("regionPrefixes");
+	}
+
+	private Map<String, String> regionTags;
+	private final IRegionControl worldGuard;
+}
--- src/no/runsafe/nchatregions/nChatRegions.java
+++ src/no/runsafe/nchatregions/nChatRegions.java
@@ -1,26 +1,12 @@
 package no.runsafe.nchatregions;
 
-import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.RunsafeConfigurablePlugin;
 
-public class nChatRegions extends RunsafePlugin
-//public class nChatRegions extends RunsafeConfigurablePlugin
+public class nChatRegions extends RunsafeConfigurablePlugin
 {
-//	public static IDebug Debugger = null;
-
 	@Override
 	protected void pluginSetup()
 	{
-//		Debugger = getComponent(IDebug.class);
-
-		// Framework features
-//		addComponent(Commands.class);
-//		addComponent(Database.class);
-//		addComponent(Events.class);
-//		addComponent(FrameworkHooks.class);
-//		addComponent(LUAScripts.class);
-//		addComponent(UniverseRegistration.class);
-
-		// Plugin components
-//		addComponent(SomeComponent.class);
+		addComponent(RegionPrefixInjector.class);
 	}
 }
