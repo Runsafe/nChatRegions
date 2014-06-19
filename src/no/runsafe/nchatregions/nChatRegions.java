efe2bbf6bbb7c10290cdb12f5bfb589770bfaea0
Morten Nilsen <morten@runsafe.no>
2014-06-19 23:53:53 +0200

    Initialized new plugin nChatRegions from template

---
+++ src/no/runsafe/nchatregions/nChatRegions.java
@@ -0,0 +1,26 @@
+package no.runsafe.nchatregions;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class nChatRegions extends RunsafePlugin
+//public class nChatRegions extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
